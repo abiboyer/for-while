4b8f11a8ae31096c6a654f9274ecfa6e78703e3d
abiboyer <70119006+abiboyer@users.noreply.github.com>
2020-10-08 13:20:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var wrapperEle = document.body.querySelector(".wrapper");
+var playerhp: 5
+var dragonhp: 10
+
+while(number<=5{
+      repeat=prompt("hit attempts (up to 5)"
+      number++")
+      }
+var playerdamage = math.floor(math.random * playerdamage +1);
+var dragondamage = math.floor(math.random* dragondamage 2||1);
+
+playerhp = playerhp - dragondamage;
+dragonhp = dragonhp - playerdamage;
+
+if (dragonhp <= 0)
+  document.body.queryselector.innerhtml=
+"You've won, congratulations!";
+
+if (playerhp <= 0)
+  document.body.queryselector.innerhtml=
+"Better luck next time."
